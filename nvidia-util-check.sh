f1f278646629c9e232bd9d1b99eaee51d1e90dd9
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2021-03-08 23:32:20 -0600

    Update nvidia-util-check.sh

--- nvidia-util-check.sh
+++ nvidia-util-check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 util=$(nvidia-smi -q -d UTILIZATION | awk '/Gpu/ { print $3 }')
+echo "checking gpu utilization"
 if [ $util -lt 80 ]; then
   echo "restarting container"
   cd /extra/mining/bin
