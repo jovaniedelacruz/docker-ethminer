aaef5c4e46d21b2b91ca6c1f0bf339db28ed32a9
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2021-03-09 01:15:26 -0600

    Update nvidia-util-check.sh

--- nvidia-util-check.sh
+++ nvidia-util-check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 util=$(nvidia-smi -q -d UTILIZATION | awk '/Gpu/ { print $3 }')
 if [ $util -lt 80 ]; then
-  echo "GPU is underutilized; Restarting container"
+  pkill ethminer
   exit 1
 else
-  echo $(date '+%Y %b %d %H:%M') Continuing
+  echo $(date '+%Y %b %d %H:%M') Continuing with GPU Util $util
   exit 1
 fi
