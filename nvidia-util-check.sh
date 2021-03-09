8885e6b2bb599759ea3d74b5a966b0aafa635324
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2021-03-08 23:03:14 -0600

    Create nvidia-util-check.sh

---
+++ nvidia-util-check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+util=$(nvidia-smi -q -d UTILIZATION | awk '/Gpu/ { print $3 }')
+if [ $util -lt 80 ]; then
+  echo "restarting container"
+  cd /extra/mining/bin
+  /usr/local/bin/docker-compose restart
+else
+  echo "continuing"
+fi
