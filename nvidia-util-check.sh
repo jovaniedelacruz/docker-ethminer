6f28ec890f6754991ca0b3d416ab96dbd58be996
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2021-03-09 00:11:03 -0600

    Update nvidia-util-check.sh

--- nvidia-util-check.sh
+++ nvidia-util-check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 util=$(nvidia-smi -q -d UTILIZATION | awk '/Gpu/ { print $3 }')
-echo "checking gpu utilization"
+echo $(date '+%Y %b %d %H:%M') Checking GPU Util
 if [ $util -lt 80 ]; then
-  echo "restarting container"
+  echo echo $(date '+%Y %b %d %H:%M') Restarting container
   cd /extra/mining/bin
   /usr/local/bin/docker-compose restart
 else
-  echo "continuing"
+  echo echo $(date '+%Y %b %d %H:%M') Continuing
 fi
