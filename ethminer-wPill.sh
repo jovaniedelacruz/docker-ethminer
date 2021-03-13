92d88de1548003115844850a4f090020d627da79
Jovanie De La Cruz <jovaniedelacruz@gmail.com>
2021-03-12 18:36:33 -0600

    Update ethminer-wPill.sh

--- ethminer-wPill.sh
+++ ethminer-wPill.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 /usr/local/bin/ethminer -U --HWMON 2 --tstart ${GPU_TEMP_START} --tstop ${GPU_TEMP_STOP} --exit --api-port -${ETHMINER_API_PORT} -P ${STRATUM}://${WALLET_ADDR}.${WORKER}:${WORKER_PASS}@${MINING_POOL}:${MINING_POOL_PORT}
+sleep 30   
 /usr/local/bin/OhGodAnETHlargementPill-r2
